552d55b83a1f33186168233c54f288a5598c9371
IraidaRomeroConde <iraidaromero18@gmail.com>
2025-10-21 00:30:17 +0200

    Todos los cambios

---
+++ js/menu.js
@@ -0,0 +1,36 @@
+const menuLateral = document.getElementById("menu-lateral");
+const fondoOscuro = document.getElementById("fondo-oscuro");
+const botonAbrir = document.getElementById("abrir-menu");
+const botonCerrar = document.getElementById("cerrar-menu");
+
+botonAbrir.addEventListener("click", () => {
+  menuLateral.classList.add("activo");
+  fondoOscuro.classList.add("activo");
+});
+
+botonCerrar.addEventListener("click", () => {
+  menuLateral.classList.remove("activo");
+  fondoOscuro.classList.remove("activo");
+});
+
+fondoOscuro.addEventListener("click", () => {
+  menuLateral.classList.remove("activo");
+  fondoOscuro.classList.remove("activo");
+});
+
+/* Submenú */
+const itemCategorias = document.querySelector(".item-categorias");
+const botonSubmenu = itemCategorias.querySelector(".boton-submenu");
+
+botonSubmenu.addEventListener("click", () => {
+  itemCategorias.classList.toggle("abierto");
+});
+
+const enlacesMenu = document.querySelectorAll(".lista-menu a");
+
+enlacesMenu.forEach(enlace => {
+  enlace.addEventListener("click", () => {
+    menuLateral.classList.remove("activo");
+    fondoOscuro.classList.remove("activo");
+  });
+});
